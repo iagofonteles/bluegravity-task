9e6bbb0f597ce61d187ce6755af955b3bdd46b52
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-19 17:50:51 -0300

    added seachprovider wrapper utility

---
+++ Assets/Scripts/Utility/Search/ISearchSettings.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utility
+{
+    public interface ISearchSettings
+    {
+        string Title { get; }
+        IEnumerable GetItens(object target);
+        string GetName(object obj);
+    }
+
+    public interface ISearchSettings<T> : ISearchSettings
+    {
+        new IEnumerable<T> GetItens(object target);
+        string GetName(T obj);
+
+        IEnumerable ISearchSettings.GetItens(object target) => GetItens(target);
+        string ISearchSettings.GetName(object obj) => GetName((T)obj);
+    }
+}
