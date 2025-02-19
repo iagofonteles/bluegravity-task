64e23126d42e6f9b104e5834898c271b6d01be43
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-19 17:57:57 -0300

    added game static acess point

---
+++ Assets/Scripts/BlueGravity/Game.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using UnityEngine;
+using Utility;
+
+namespace BlueGravity
+{
+    public static class Game
+    {
+        public static GameSave Save;
+        
+        static Game()
+        {
+            var saveFolder = Path.Combine(Application.persistentDataPath, "Saves");
+            Save = new GameSave(saveFolder);
+        }
+    }
+}
