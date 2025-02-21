4a88fab820d705ac1736324cddaf3a746a6ba9d4
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-21 19:53:16 -0300

    added items to addressables

---
+++ Assets/Scripts/InventoryView/SlotController.cs
@@ -0,0 +1,21 @@
+using UnityEngine.EventSystems;
+using ViewUtility;
+
+namespace Inventory.UI
+{
+    public class InventoryItemCountController : DataView<ISlot>
+    {
+        protected override void Subscribe(ISlot data) { }
+        protected override void Unsubscribe(ISlot data) { }
+
+        public void SetItem(PointerEventData e)
+            => SetItem(e.pointerDrag.GetComponent<DataView>());
+
+        public void SetItem(DataView view)
+        {
+            var data = view?.GetData();
+            if (data is ISlot slot) Data.Item = slot.Item;
+            else Data.Item = data;
+        }
+    }
+}
