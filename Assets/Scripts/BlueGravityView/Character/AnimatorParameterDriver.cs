2fddd31094de21e73161a5f19009a321b9a7af70
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-22 18:40:35 -0300

    fix collision bounce

--- Assets/Scripts/BlueGravityView/Character/AnimatorParameterDriver.cs
+++ Assets/Scripts/BlueGravityView/Character/AnimatorParameterDriver.cs
@@ -1,4 +1,4 @@
-using System;
+using BlueGravity;
 using UnityEngine;
 
 namespace BlueGravityView
@@ -7,17 +7,24 @@ namespace BlueGravityView
     {
         public Transform model;
         public Animator animator;
+        public Character character;
         public string speed = "speed";
-        public float maxSpeed = 5;
+        public float threshold = 2f;
 
         private Vector3 _lastPosition;
-
 
         private void Update()
         {
             var delta = model.position - _lastPosition;
             _lastPosition = model.position;
-            animator.SetFloat(speed, delta.magnitude / (maxSpeed * Time.deltaTime));
+
+            var moveSpeed = character.Player.Stats.MoveSpeed.Value;
+
+            var animSpeed = 0f;
+            if (delta.magnitude > .05)
+                animSpeed = moveSpeed > threshold ? 2 : 1;
+
+            animator.SetFloat(speed, animSpeed);
         }
     }
 }
