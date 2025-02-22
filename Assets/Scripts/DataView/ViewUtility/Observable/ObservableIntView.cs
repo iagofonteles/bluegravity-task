4d9cbca20ed886eae67ed0d1db53333784d1224e
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-22 14:51:27 -0300

    observable view actions

--- Assets/Scripts/DataView/ViewUtility/Observable/ObservableIntView.cs
+++ Assets/Scripts/DataView/ViewUtility/Observable/ObservableIntView.cs
@@ -1,4 +1,10 @@
 namespace ViewUtility
 {
-    public class ObservableIntView : ObservableViewT<int> { }
+    public class ObservableIntView : ObservableViewT<int>
+    {
+        public void Add(int value) => Data.Value += value;
+        public void Subtract(int value) => Data.Value -= value;
+        public void Multiply(int value) => Data.Value *= value;
+        public void Divide(int value) => Data.Value /= value;
+    }
 }
