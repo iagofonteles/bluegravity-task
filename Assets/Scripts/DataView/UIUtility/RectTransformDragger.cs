673ec7f697e7bcf8a4d2b0763856455d3427bf99
Iago Fonteles <iagofonteles1@gmail.com>
2025-02-25 09:14:19 -0300

    support for other resolutions

--- Assets/Scripts/DataView/UIUtility/RectTransformDragger.cs
+++ Assets/Scripts/DataView/UIUtility/RectTransformDragger.cs
@@ -6,24 +6,30 @@ namespace UIUtility
     public class RectTransformDragger : MonoBehaviour, IBeginDragHandler, IDragHandler
     {
         public RectTransform target;
-        [Min(-1)] public Vector2 border;
+        public Vector2 border;
 
         private Vector2 _min, _max;
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            var canvas = target.GetComponentInParent<Canvas>();
-            _min = border + target.sizeDelta * target.pivot;
-            _max = new Vector2(Screen.width, Screen.height) * canvas.scaleFactor; // TODO test with canvas scale
-            _max -= border + target.sizeDelta * (Vector2.one - target.pivot);
+            var scale = target.GetComponentInParent<Canvas>().scaleFactor;
+            var pivot = new Vector2(target.pivot.x, target.pivot.y);
+            var minSize = target.sizeDelta * pivot;
+            var maxSize = target.sizeDelta * (Vector2.one - pivot);
+
+            _min = new(0, -Screen.height / scale);
+            _min += border + minSize;
+            
+            _max = new Vector2(Screen.width / scale, 0);
+            _max -= border + maxSize;
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            var pos = target.position;
+            var pos = target.anchoredPosition;
             pos.x = Mathf.Clamp(pos.x + eventData.delta.x, _min.x, _max.x);
             pos.y = Mathf.Clamp(pos.y + eventData.delta.y, _min.y, _max.y);
-            target.position = pos;
+            target.anchoredPosition = pos;
         }
     }
 }
